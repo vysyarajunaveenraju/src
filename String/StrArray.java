4f91dae28931eba293feda6689aa20b27ceb5445
vysyarajunaveenraju <vysyarajunaveenraju@gmail.com>
2024-09-10 20:09:00 +0530

    adding new

---
+++ String/StrArray.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StrArray {
+    public static void main(String[] args) {
+        String str = "Asish is Prabhas Fan boy";
+        String arr [] = str.split(" ");
+        for(String i :  arr)
+            System.out.print(i+" ");
+    }
+    
+}
