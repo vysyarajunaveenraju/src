87508d3a72ed10c6b56d9c7fbf82bd12a584751b
vysyarajunaveenraju <vysyarajunaveenraju@gmail.com>
2024-09-10 20:03:54 +0530

    adding begin

---
+++ Wordlength.java
@@ -0,0 +1,17 @@
+public class Wordlength {
+    public static void main(String[] args) {
+        String sentence = "I am Naveen raju Vysyaraju";
+        System.out.print(wordLength(sentence));
+    }
+    public static String wordLength(String sentence)
+    {
+        String [] arr = sentence.split(" ");
+        StringBuilder sb  = new StringBuilder();
+        for(int i=0;i<arr.length;i++)
+        {
+            sb.append(arr[i].length()).append(" ");
+        }
+        return sb.toString();
+    }
+    
+}
